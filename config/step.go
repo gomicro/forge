a4ae5fed79b8daccf7c7626a736ce239a36d3802
Daniel Hess <dan9186@gmail.com>
2021-03-08 20:41:44 -1000

    simplify support for piped commands

--- config/step.go
+++ config/step.go
@@ -1,13 +1,29 @@
 package config
 
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
 // Step represents details of single step to be executed by the cli.
 type Step struct {
-	Cmd Cmd `yaml:"cmd"`
+	Cmd string `yaml:"cmd"`
 	//Cmds []string `yaml:"cmds"`
 }
 
 // Execute runs the command that is specified for the step. It returns the output
 // of the command and any errors it encounters.
 func (s *Step) Execute() (string, error) {
-	return s.Cmd.Execute()
+	cmd := exec.Command("bash", "-c", s.Cmd)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("cmd exec: %v", err.Error())
+	}
+
+	return out.String(), nil
 }
